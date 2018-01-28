994881a932992a78f1949955507f51cd928d5b13
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-01-28 11:48:30 +0300

    Update tkm.py

--- tkm.py
+++ tkm.py
@@ -1,16 +1,40 @@
 import random
+import time
+import os
 bscm = ['tas','kagit','makas']
-skr = 3
-
-
+skr = -1
 k = str("k")
 m = str("m")
 t = str("t")
+E = str("E")
+e = str("e")
+h = str("h")
+H = str("H")
+if(skr <0):
+    print("KAYBETTINIZ")
+    print("Tekrar oynamak istermisiniz ? [E/H]")
+    tkr = str(input())
+    if((tkr=="E") or (tkr=="e")):
+        skr = 3
+        os.system
+        ('clear')
+    if((tkr == "h") or (tkr == "H")):
+        os.system('exit')
+        os.system('exit')
+os.system('clear')
+print("t = tas -- k = makas -- ")
 while skr >= 0:
+    print("\n \n \n")
     bot = random.choice(bscm)
     if(bot == "tas"):
         print("Seciminiz : ")
         kscm = str(input())
+        print("tas...")
+        time.sleep(0.4)
+        print("kagit...")
+        time.sleep(0.4)
+        print("makas...")
+        time.sleep(0.4)
         if(kscm == t):
             print("!BERABERE!")
             print "Skorunuz : ", skr
@@ -23,11 +47,15 @@ while skr >= 0:
             skr -=1
             print "Skorunuz : ", skr
 
-
-
-    if(bot == "kagit"):
+    elif(bot == "kagit"):
         print("Seciminiz : ")
         kscm = str(input())
+        print("tas...")
+        time.sleep(0.4)
+        print("kagit...")
+        time.sleep(0.4)
+        print("makas...")
+        time.sleep(0.4)
         if(kscm == k):
             print("!BERABERE!")
             print "Skorunuz : ", skr
@@ -40,11 +68,15 @@ while skr >= 0:
             skr -=1
             print "Skorunuz : ", skr
 
-
-
-    if(bot == "makas"):
+    elif(bot == "makas"):
         print("Seciminiz : ")
         kscm = str(input())
+        print("tas...")
+        time.sleep(0.4)
+        print("kagit...")
+        time.sleep(0.4)
+        print("makas...")
+        time.sleep(0.4)
         if(kscm == m):
             print("!BERABERE!")
             print "Skorunuz : ", skr
@@ -56,5 +88,9 @@ while skr >= 0:
             print("!KAYBETTINIZ!")
             skr -=1
             print "Skorunuz : ", skr
-    print "Botun sectigi " , bot , " idi "
+    else:
+        print("Seciminiz : ")
+        kscm = str(input())
+    print "Botun sectigi ->" , bot , "<- idi "
+
 
